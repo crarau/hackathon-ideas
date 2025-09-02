1257a236a8516e9e10ed7e8a2801e714c3c60000
Chip <ciprarau@gmail.com>
2025-09-02 01:28:00 +0000

    Update domain to hackathon-ideas.ciprianrarau.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://hackathon.tprl.com',
+  url: 'https://hackathon-ideas.ciprianrarau.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
